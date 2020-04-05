74c26fe013a290265162b1adbc36c72a8c68a155
B-raw <michaelbarry1989@hotmail.com>
2020-04-05 21:12:38 +0100

    GraphQL Yoga set up with Items

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,12 +1,25 @@
 const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+    console.log({...args})
+    console.log(info)
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info)
 
-  createDog(parent, args, ctx, info) {
-    global.dogs = global.dogs || []
-    //create a dog!
-    const newDog = { name: args.name };
-    global.dogs.push(newDog);
-    return newDog;
-    }
+    return item;
+  }
+
+
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || []
+  //   //create a dog!
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   return newDog;
+  //   }
 
 };
 
