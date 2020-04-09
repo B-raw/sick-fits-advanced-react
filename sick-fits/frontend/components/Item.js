e57f305210baaacdc9a7a40f5c9bb91bb751627b
B-raw <michaelbarry1989@hotmail.com>
2020-04-09 23:14:15 +0100

    added first apollo query to react - get Items, and list out Item Components

---
+++ sick-fits/frontend/components/Item.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+// import Title from './styles/title';
+import ItemStyles from './styles/ItemStyles';
+import PriceTag from './styles/PriceTag';
+import formatMoney from '../lib/formatMoney';
+
+class Item extends Component {
+  static propTypes = {
+    item: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+      description: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired,
+      largeImage: PropTypes.string.isRequired
+    })
+  }
+
+  render() {
+    const { item } = this.props;
+    console.log(formatMoney(item.price))
+    return (
+      <ItemStyles>
+        { item.image && <img src={item.image} alt={item.title}/> }
+        {/* <Title> */}
+          <Link href={{
+            pathname: '/item',
+            query: { id: item.id },
+          }}>
+            <a>{item.title}</a>
+          </Link>
+          <PriceTag style={{color: "black"}}>
+            <p>{formatMoney(item.price)}</p>
+          </PriceTag>
+          <p>{item.description}</p>
+          <div className="buttonList">
+            <Link href={{
+              pathname:"update",
+              query: { id: item.id }
+            }}>
+              <a>Edit ✏️</a>
+            </Link>
+            <button>Add To Cart</button>
+            <button>Delete</button>
+          </div>
+        {/* </Title> */}
+      </ItemStyles>
+    );
+  }
+
+}
+
+export default Item;
--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -2,11 +2,20 @@ import Link from 'next/link';
 
 const Nav = ({}) => (
   <div>
+  <Link href="/items">
+    <a>Shop</a>
+  </Link>
     <Link href="/sell">
-      <a>Sell!</a>
+      <a>Sell</a>
     </Link>
-    <Link href="/">
-      <a>Home!</a>
+    <Link href="/signup">
+      <a>Signup</a>
+    </Link>
+    <Link href="/orders">
+      <a>Orders</a>
+    </Link>
+    <Link href="/me">
+      <a>Account</a>
     </Link>
   </div>
 );
