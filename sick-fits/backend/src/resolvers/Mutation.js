986808a9414ae6025cdc438079f42e66cca19f9b
B-raw <michaelbarry1989@hotmail.com>
2020-05-24 16:17:27 +0100

    added cart and mutation to add cartItem

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -200,6 +200,42 @@ const Mutations = {
       },
     }, info);
   },
+
+  async addToCart(parent, args, ctx, info) {
+    // 1. ensure they are signed in
+    const { userId } = ctx.request;
+    if(!userId) {
+      throw new Error('You must be signed in');
+    }
+    // 2. query the users current cart
+    const [existingCartItem] = await ctx.db.query.cartItems({
+      where: {
+        user: { id: userId },
+        item: { id: args.id },
+      }
+    });
+    // 3. check if that item is already in their cart and incremenet by one if it is
+    if(existingCartItem) {
+      console.log("this item is already in the cart");
+      return ctx.db.mutation.updateCartItem({
+        where: { id: existingCartItem.id },
+        data: { quantity: existingCartItem.quantity + 1 }
+      }, info);
+    }
+    //4. if its not, create a fresh cartItem for that user!
+    return ctx.db.mutation.createCartItem({
+      data: {
+        user: {
+          connect: { id: userId },
+        },
+        item: {
+          connect: { id: args.id },
+        }
+      }
+    },
+    info
+  );
+  },
 };
 
 module.exports = Mutations;
